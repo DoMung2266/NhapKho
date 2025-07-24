b4013f46043cecdaf0532128c7aa99463982444b
DoMung2266 <nhamayvanmung@gmail.com>
2025-07-24 16:37:56 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// Đã được tách sẵn trong config.js:
+const GITHUB_USERNAME = "DoMung2266";
+const GITHUB_REPO = "NhapKho";
+const JSON_PATH = "products.json";
+const apiUrl = `https://api.github.com/repos/${GITHUB_USERNAME}/${GITHUB_REPO}/contents/${JSON_PATH}`;
